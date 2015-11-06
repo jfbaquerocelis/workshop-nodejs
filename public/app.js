eba050aa8f10ea7254b6f92639f052b2844e9c81
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-11-05 20:41:19 -0500

    agregamos manejo de uris para servir archivo javascript

---
+++ public/app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.aniversary')
+
+button.addEventListener('click', function () {
+	console.log('Felicidades Comunidad!')
+	alert('Felicidades Comunidad!')
+})
--- server.js
+++ server.js
@@ -13,16 +13,33 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest(req, res){
-	let index = path.join(__dirname, 'public', 'index.html')
-	let reader = fs.createReadStream(index)
+	let uri = req.url
 
-	res.setHeader('Content-Type', 'text/html')
-	reader.pipe(res)
+	if (uri.startsWith('/index') || uri === '/'){
+		let index = path.join(__dirname, 'public', 'index.html')
+		let reader = fs.createReadStream(index)
+		
+		res.setHeader('Content-Type', 'text/html')
+		reader.pipe(res)
 
-	reader.on('error', function (err){
-		res.setHeader('Content-Type', 'text/plain')
-		res.end(err.message)
-	})
+		reader.on('error', function (err){
+			res.setHeader('Content-Type', 'text/plain')
+			res.end(err.message)
+		})
+	}
+
+	if (uri.startsWith('/app.js')){
+		let app = path.join(__dirname, 'public', 'app.js')
+		let reader = fs.createReadStream(app)
+
+		res.setHeader('Content-Type', 'text/javascript')
+		reader.pipe(res)
+
+		reader.on('error', function (err){
+			res.setHeader('Content-Type', 'text/plain')
+			res.end(err.message)
+		})
+	}
 }
 
 function onListening(){
