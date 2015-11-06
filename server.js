47115c4c7a566ad18356dccc39d6dfdd36c1c8b0
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-11-05 19:35:50 -0500

    agregamos callbacks como function named a los métodos asincronos del servidor

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const http = require('http')
 const port = 3000
 
-const server = http.createServer()
+const server = http.createServer(onRequest)
 
-server.listen(port)
+server.listen(port, onListening)
+
+function onRequest(req, res){
+	res.end('Hola MonteriaJS')
+}
+
+function onListening(){
+	console.log(`Servidor corriendo en puerto ${port}`)
+}
