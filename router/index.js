a3f8919326c0053c35abe19169c7b999dc873b40
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-11-08 15:11:35 -0500

    Agregamos manejo de rutas en nuestro servidor estático

--- router/index.js
+++ router/index.js
@@ -1,17 +1,37 @@
 const path = require('path')
 const st = require('st')
+const Router = require('router')
+
+const router = Router()
 
 const mount = st({
 	path : path.join(__dirname, '..', 'public'),
-	index : 'index.html'
+	index : 'index.html',
+	passthrough : true
+})
+
+const monteriajs = {
+	"name" : "MonteríaJS",
+	"city" : "Montería",
+	"description" : "Comunidad de Javascript y Desarrollo Web",
+	"web" : "http://meetup.com/monteriajs"
+}
+
+router.get('/monteriajs', function (req, res){
+	res.setHeader('Content-Type', 'application/json; charset=utf-8')
+	res.end(JSON.stringify(monteriajs))
 })
 
 function serveStatic (req, res){
 	mount(req, res, function (err){
 		if (err) return fail(err, res)
+		
+		router(req, res, function (err){
+			if (err) return fail(err, res)
 
-		res.statusCode = 404
-		res.end(`404 Not Found: ${req.url}`)
+			res.setHeader('Content-Type', 'text/plain')
+			res.end(`404 Not Found: ${req.url}`)
+		})
 	})
 }
 
