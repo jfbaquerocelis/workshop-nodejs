3c5125873d7bdb0e7a302cfb311950f7d83eddc0
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-11-08 10:52:26 -0500

    Creación de servidor de archivos estaticos con st

--- server.js
+++ server.js
@@ -2,45 +2,15 @@
 
 const http = require('http')
 const fs = require('fs')
-const path = require('path')
+const serveStatic = require('./router')
 const port = 3000
 
 const server = http.createServer()
 
-server.on('request', onRequest)
+server.on('request', serveStatic)
 server.on('listening', onListening)
 
 server.listen(port)
-
-function onRequest(req, res){
-	let uri = req.url
-
-	if (uri.startsWith('/index') || uri === '/'){
-		let index = path.join(__dirname, 'public', 'index.html')
-		let reader = fs.createReadStream(index)
-		
-		res.setHeader('Content-Type', 'text/html')
-		reader.pipe(res)
-
-		reader.on('error', function (err){
-			res.setHeader('Content-Type', 'text/plain')
-			res.end(err.message)
-		})
-	}
-
-	if (uri.startsWith('/app.js')){
-		let app = path.join(__dirname, 'public', 'app.js')
-		let reader = fs.createReadStream(app)
-
-		res.setHeader('Content-Type', 'text/javascript')
-		reader.pipe(res)
-
-		reader.on('error', function (err){
-			res.setHeader('Content-Type', 'text/plain')
-			res.end(err.message)
-		})
-	}
-}
 
 function onListening(){
 	console.log(`Servidor corriendo en puerto ${port}`)
